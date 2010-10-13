c6b2c097358ab9d1b7c6c75188ff29a0d5c504a7
Tom <devnull@localhost>
2010-10-13 12:05:40 +0000

    StringHashTool:  - A quick tool for hashing text using the Volition hash and CRC algorithms

---
+++ StringHashTool/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace StringHashTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CrcVolitionTextBox = new System.Windows.Forms.TextBox();
+            this.HashVolitionTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String:";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputTextBox.Location = new System.Drawing.Point(55, 12);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(217, 186);
+            this.InputTextBox.TabIndex = 1;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CrcVolition:";
+            // 
+            // CrcVolitionTextBox
+            // 
+            this.CrcVolitionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.CrcVolitionTextBox.Location = new System.Drawing.Point(87, 230);
+            this.CrcVolitionTextBox.Name = "CrcVolitionTextBox";
+            this.CrcVolitionTextBox.ReadOnly = true;
+            this.CrcVolitionTextBox.Size = new System.Drawing.Size(185, 20);
+            this.CrcVolitionTextBox.TabIndex = 3;
+            // 
+            // HashVolitionTextBox
+            // 
+            this.HashVolitionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.HashVolitionTextBox.Location = new System.Drawing.Point(87, 204);
+            this.HashVolitionTextBox.Name = "HashVolitionTextBox";
+            this.HashVolitionTextBox.ReadOnly = true;
+            this.HashVolitionTextBox.Size = new System.Drawing.Size(185, 20);
+            this.HashVolitionTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "HashVolition:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.HashVolitionTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CrcVolitionTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "String hash tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CrcVolitionTextBox;
+        private System.Windows.Forms.TextBox HashVolitionTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StringHashTool/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Gibbed.SaintsRow2.Helpers;
+
+namespace StringHashTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            CrcVolitionTextBox.Text = String.Format("{0:X8}", StringHelpers.CrcVolition(InputTextBox.Text));
+            HashVolitionTextBox.Text = String.Format("{0:X8}", StringHelpers.HashVolition(InputTextBox.Text));
+        }
+    }
+}
