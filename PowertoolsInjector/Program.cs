fd39e92a1ec80316a2165cbf9b1c26ab0e86335f
Thomas Jepp <tom@tomjepp.co.uk>
2011-06-25 22:51:06 +0100

    Backed out changeset: 94dea91b7eb4

--- PowertoolsInjector/Program.cs
+++ PowertoolsInjector/Program.cs
@@ -1,38 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
+using System.Linq;
 using System.Text;
-
-using EasyHook;
-using ManagedPowertools.Server;
 
 namespace PowertoolsInjector
 {
-    public static class Program
+    class Program
     {
-        private static string ChannelName = null;
-
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            try
-            {
-                string directory = Utility.GetAssemblyDirectory();
-                string injectorPath = Path.Combine(Utility.GetAssemblyDirectory(), "PowertoolsInjector.exe");
-                string managedPowertoolsPath = Path.Combine(Utility.GetAssemblyDirectory(), "ManagedPowertools.dll");
-                Config.Register("Saints Row 2 Managed Powertools", injectorPath, managedPowertoolsPath);
-                RemoteHooking.IpcCreateServer<PowertoolsInterface>(ref ChannelName, System.Runtime.Remoting.WellKnownObjectMode.SingleCall);
-
-                int processId = -1;
-
-                RemoteHooking.CreateAndInject("SR2_pc.exe", "", 0, "ManagedPowertools.dll", "ManagedPowertools.dll", out processId, ChannelName);
-
-                Console.ReadLine();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Failed to connect to target.\nException:\n{0}", ex);
-            }
         }
     }
 }
