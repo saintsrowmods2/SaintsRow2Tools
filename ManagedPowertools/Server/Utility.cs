b28c6fcbf32c79f0726b022ed60c4a633285ecbc
Thomas Jepp <tom@tomjepp.co.uk>
2011-06-25 18:13:51 +0100

    More ManagedPowertools implementation

---
+++ ManagedPowertools/Server/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagedPowertools.Server
+{
+    public static class Utility
+    {
+        public static string GetAssemblyPath()
+        {
+            return System.Reflection.Assembly.GetCallingAssembly().Location;
+        }
+
+        public static string GetAssemblyDirectory()
+        {
+            return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetCallingAssembly().Location);
+        }
+    }
+}
--- PowertoolsInjector/Program.cs
+++ PowertoolsInjector/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
+
+using EasyHook;
+using ManagedPowertools.Server;
 
 namespace PowertoolsInjector
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static string ChannelName = null;
+
+        public static void Main(string[] args)
         {
+            try
+            {
+                string directory = Utility.GetAssemblyDirectory();
+                string injectorPath = Path.Combine(Utility.GetAssemblyDirectory(), "PowertoolsInjector.exe");
+                string managedPowertoolsPath = Path.Combine(Utility.GetAssemblyDirectory(), "ManagedPowertools.dll");
+                Config.Register("Saints Row 2 Managed Powertools", injectorPath, managedPowertoolsPath);
+                RemoteHooking.IpcCreateServer<PowertoolsInterface>(ref ChannelName, System.Runtime.Remoting.WellKnownObjectMode.SingleCall);
+
+                int processId = -1;
+
+                RemoteHooking.CreateAndInject("SR2_pc.exe", "", 0, "ManagedPowertools.dll", "ManagedPowertools.dll", out processId, ChannelName);
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect to target.\nException:\n{0}", ex);
+            }
         }
     }
 }
